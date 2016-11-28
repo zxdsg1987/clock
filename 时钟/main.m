af91c7d53b136c5f77280faba0ba2bec78b37997
skyWalker <zxdsg1987@qq.com>
2016-11-28 10:10:58 +0800

    时钟
    根据当前系统时间自己走

---
+++ 时钟/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  时钟
+//
+//  Created by skywalker on 16/11/28.
+//  Copyright © 2016年 斯芬克斯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
